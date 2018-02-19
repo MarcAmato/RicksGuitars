d9f5a39295763fdf95a40125e6fa357aa433f2b8
Marc Amato <marc.amato@hotmail.com>
2018-02-19 12:08:05 +0800

    Step 2: Apply OO Principles

--- src/FindGuitarTester.java
+++ src/FindGuitarTester.java
@@ -2,6 +2,9 @@
 //  -> Customer comes in and tells him what they like,
 //  -> Rick runs a search on his inventory
 
+import java.util.Iterator;
+import java.util.List;
+
 public class FindGuitarTester {
 
     public static void main(String[] args) {
@@ -9,17 +12,26 @@ public class FindGuitarTester {
         Inventory inventory = new Inventory();
         initializeInventory(inventory);
 
-        Guitar whatErinLikes = new Guitar("", 0.0, Builder.FENDER, "Stratocaster",
+        Guitar whatErinLikes = new Guitar("", 0, Builder.FENDER, "Stratocaster",
                                             Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
 
-        Guitar guitar = inventory.search(whatErinLikes);
-        if (guitar != null) {
-            System.out.println("Erin, you might like this " +
-                guitar.getBuilder() + " " + guitar.getModel() + " " +
-                guitar.getType() + " guitar:\n " +
-                guitar.getBackWood() + " back and sides,\n " +
-                guitar.getTopWood() + " top.\nYou can have it for only $" +
-                guitar.getPrice() + "!");
+        inventory.addGuitar("", 0, Builder.FENDER, "Stratocaster",
+                Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
+
+        // Get the whole list of guitars that match the clients specs.
+        List matchingGuitars = inventory.search(whatErinLikes);
+        // Iterate over all the choices returned from the search tool.
+        if (!matchingGuitars.isEmpty()) {
+            System.out.println("Erin, you might like these guitars:");
+            for (Iterator i = matchingGuitars.iterator(); i.hasNext(); ) {
+                Guitar guitar = (Guitar)i.next();
+                System.out.println("We have a " +
+                        guitar.getBuilder() + " " + guitar.getModel() + " " +
+                        guitar.getType() + " guitar:\n " +
+                        guitar.getBackWood() + " back and sides,\n " +
+                        guitar.getTopWood() + " top.\n You can have it for only $" +
+                        guitar.getPrice() + "!\n ----");
+            }
         } else {
             System.out.println("Sorry, Erin, we have nothing for you.");
         }
